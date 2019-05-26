a2ff92e4b5842bda0006d72a8e9f093e0d60fbd3
Ryan <rrichetto@comcast.net>
2019-05-26 18:23:53 -0500

    adds Form section changing functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,6 +32,62 @@ function closeModal() {
 const prevBtnEl = document.querySelector('.form__prev');
 const nextBtnEl = document.querySelector('.form__next');
 const sectionEls = document.querySelectorAll('.form__section');
-const dotEls = document.querySelectorAll('.form__dot')
-const submitEl = document.querySelector('.form__submit')
+const dotEls = document.querySelectorAll('.form__dot');
+const submitEl = document.querySelector('.form__submit');
 
+let currentSection = 0;
+
+nextBtnEl.addEventListener('click', _ => {
+  currentSection++;
+  displaySection();
+});
+
+prevBtnEl.addEventListener('click', _ => {
+  currentSection--;
+  displaySection();
+});
+
+function displaySection() {
+  sectionEls.forEach((section, index) => {
+    if (index === currentSection) {
+      section.style.display = 'block';
+    } else {
+      section.style.display = 'none';
+    }
+  })
+
+  showButtons();
+  showDots();
+}
+
+function showButtons() {
+  if (currentSection === 0) {
+    prevBtnEl.style.display = 'none';
+    nextBtnEl.style.display = 'block';
+    submitEl.style.display = 'none';
+  }
+  if (currentSection === 1) {
+    prevBtnEl.style.display = 'block';
+    nextBtnEl.style.display = 'block';
+    submitEl.style.display = 'none';
+  }
+  if (currentSection === 2) {
+    nextBtnEl.style.display = 'block';
+    nextBtnEl.style.display = 'none';
+    submitEl.style.display = 'block';
+  }
+}
+
+function showDots() {
+  dotEls.forEach((dot, index) => {
+    if (index === currentSection) {
+      dot.style.height = '1.75rem';
+      dot.style.width = '1.75rem';
+      dot.style.backgroundColor = '#3ECF8E';
+    } else {
+      dot.style.height = '1.25rem';
+      dot.style.width = '1.25rem';
+      dot.style.backgroundColor = '#97ECC6';
+    }
+  })
+}
